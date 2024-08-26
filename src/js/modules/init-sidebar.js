ab9b04d19ddc2d9d2a11a911a734220c0b338e71
Сергей Новиков <sergey.novikov.zb@gmail.com>
2024-08-26 12:24:15 +0900

    script: added 'Sidebar' logic

---
+++ src/js/modules/init-sidebar.js
@@ -0,0 +1,12 @@
+import Sidebar from "../classes/Sidebar"
+
+export default function initSidebar() {
+  const sidebarElement = document.querySelector('[data-sidebar="sidebar"]')
+
+  if (!sidebarElement) {
+    return
+  }
+
+  const sidebar = new Sidebar(sidebarElement)
+  sidebar.init()
+}
