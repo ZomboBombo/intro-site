5947403fdb3e8e34774232d61cd8bb8080f7212e
Сергей Новиков <sergey.novikov.zb@gmail.com>
2024-08-26 12:25:54 +0900

    script: added new modules 'inits'

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
 import initBurgerMenu from './modules/init-burger-menu'
+import initSidebar from './modules/init-sidebar'
+import initTooltip from './modules/init-tooltip'
 
 document.addEventListener('DOMContentLoaded', () => {
-    initBurgerMenu()
+  initBurgerMenu()
+  initSidebar()
+  initTooltip()
 })
 
 /*
   В 'load' добавляются скрипты,
   не участвующие в работе первого экрана
 */
-window.addEventListener('load', () => {})
+window.addEventListener('load', () => { })
